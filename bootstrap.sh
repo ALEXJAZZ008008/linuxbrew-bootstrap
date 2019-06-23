de47626666b196eaf2ce09bde003b47a3e65e486
Alexander Whitehead <alexandercwhitehead@gmail.com>
2019-06-23 12:15:25 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,12 +20,19 @@ export HOMEBREW_LOGS=/SAN/inm/moco/aaaALEX/.log
 brew install gcc --without-glibc --only-dependencies
 
 prefix=~/.linuxbrew
+
+echo -e 'mkdir -p '"$prefix"'/Cellar/gcc/6.2.0/'
 mkdir -p "$prefix"'/Cellar/gcc/6.2.0/'
+
+echo -e 'cp -r /share/apps/gcc-6.2.0/* '"$prefix"'/Cellar/gcc/6.2.0/'
 cp -r /share/apps/gcc-6.2.0/* "$prefix"'/Cellar/gcc/6.2.0/'
 
 brew link gcc
 
+echo -e 'mkdir -p '"$prefix"'/Cellar/gcc/6.2.0/lib/gcc/x86_64-unknown-linux-gnu/6.2.0/specs.orig'
 mkdir -p "$prefix"'/Cellar/gcc/6.2.0/lib/gcc/x86_64-unknown-linux-gnu/6.2.0/specs.orig'
+
+echo -e 'gcc -dumpspecs > '"$prefix"'/Cellar/gcc/6.2.0/lib/gcc/x86_64-unknown-linux-gnu/6.2.0/specs.orig'
 gcc -dumpspecs > "$prefix"'/Cellar/gcc/6.2.0/lib/gcc/x86_64-unknown-linux-gnu/6.2.0/specs.orig'
 
 brew install -v gmp
@@ -33,10 +40,45 @@ brew install -v isl@0.18
 brew install -v libmpc
 brew install -v mpfr
 
+brew install -v m4
+brew install -v gpatch
+brew install -v ncurses
+brew install -v gettext
+brew install -v bzip2
+brew install -v pcre2
+brew install -v libbsd
+brew install -v expat
+brew install -v libffi
+brew install -v gpatch
+brew install -v ncurses
+brew install -v libedit
+brew install -v libelf
+brew install -v gdbm
+brew install -v readline
+brew install -v sqlite
+brew install -v xz
+brew install -v python
+brew install -v libxml2
+brew install -v python@2
+brew install -v libpng
+brew install -v freetype
+brew install -v berkeley-db
+brew install -v perl
+brew install -v autoconf
+brew install -v automake
+brew install -v gperf
+brew install -v libtool
+brew install -v json-c
+brew install -v util-linux
+brew install -v fontconfig, 
+
 brew install -v glibc
 
 brew unlink gcc
+
+echo -e 'rm -rf '"$prefix"'/Cellar/gcc/'
 rm -rf "$prefix"'/Cellar/gcc/'
+
 brew install -v gcc
 
 brew install -v hello
