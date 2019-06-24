42f1921991d216eb456646dc128bf7e4754e09a4
Alexander Whitehead <alexandercwhitehead@gmail.com>
2019-06-24 02:23:25 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-./download.sh
-./install.sh $1
+echo -e $(hostname)
+echo -e $(date)"\n"
+
+/usr/bin/time -f 'Elapsed time: %es\nMemory usage: %M KB\nCPU usage: %P' ./download.sh | tee ./linuxbrew-bootstrap_download_log
+/usr/bin/time -f 'Elapsed time: %es\nMemory usage: %M KB\nCPU usage: %P' ./install.sh $1 | tee ./linuxbrew-bootstrap_install_log
+
+echo -e $(date)
