6cfa1617ddb6366a03fc666f8853e87abad0536b
Alexander Whitehead <alexandercwhitehead@gmail.com>
2019-06-25 15:45:03 +0100

    Update run_run.sh

--- run_run.sh
+++ run_run.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+
+# Copyright University College London 2019
+# Author: Alexander Whitehead, Institute of Nuclear Medicine, UCL
+# For internal research only.
+
 ./run.sh $(pwd) | tee linuxbrew-bootstrap_log
