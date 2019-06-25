b036d6bcba91c159ca46b088c006e30d4902f8f3
Alexander Whitehead <alexandercwhitehead@gmail.com>
2019-06-25 15:45:32 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Copyright University College London 2019
+# Author: Alexander Whitehead, Institute of Nuclear Medicine, UCL
+# For internal research only.
+
 brew install -v llvm
 
 brew tap linuxbrew/homebrew-xorg
